7f921a9d93af49d5bcdde779b41dbbfbaf12415c
Oke Babatunde <tundejosilnet@gmail.com>
2023-11-24 23:44:23 +0100

    setting up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-white">
+      <h1 className="text-effect">Hello World</h1>
     </div>
   );
 };
