de78ffb992954c78c9e0c87ef6d1caea402108d2
Oke Babatunde <tundejosilnet@gmail.com>
2023-12-04 00:35:01 +0100

    login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,50 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsPerson } from "react-icons/bs";
+import { FaRegUser, FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import loginPic from "../../assets/login.png";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const loginUser = () => {};
+
   return (
-    <section className={`container hero min-h-screen `}>
-      <div className="flex-col hero-content lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    <section className={`container min-h-[88vh] authB bg-cl-white hero`}>
+      <div className="flex flex-col items-center ">
+        <div className="mb-20 text-center">
+          <h1 className="text-3xl font-bold md:text-5xl">Login to Your Account</h1>
         </div>
-        <div className="w-full max-w-sm shadow-2xl card shrink-0 bg-base-100">
-          <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
+
+        <div className="flex flex-row-reverse items-center justify-between">
+          <img src={loginPic} alt="login" className="hidden max-w-sm md:flex" />
+          <div className="flex flex-col items-center">
+            <div className="md:p-5 shadow-2xl glass rounded-[50px] text-cl-white flex flex-col pb-20 z-10">
+              <form
+                onSubmit={loginUser}
+                className="flex flex-col items-center text-center "
+              >
+                <div className="p-6 m-2 mb-5 -mt-16 text-5xl border-2 rounded-full bg-cl-sec w-fit text-cl-acn">
+                  <BsPerson />
+                </div>
+                <div>
+                  <div className="flex m-4 text-center">
+                    <div className="p-4 px-6 text-center bg-cl-acn">
+                      <FaRegUser className="text-xl" />
+                    </div>
+                    <input
+                      type="email"
+                      placeholder="Email ID"
+                      required
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="w-full p-2 text-xl outline-none bg-cl-acn2 text-cl-black"
+                    />
+                  </div>
+                  <div className="flex m-4 text-center">
+                    <div className="p-4 px-6 text-center bg-cl-acn">
+                      <FaLock className="text-xl" />
+                    </div>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="w-full p-2 text-xl outline-none bg-cl-acn2 text-cl-black"
+                    />
+                    <div className="p-4 text-center bg-cl-acn2">
+                      <FaLock className="text-xl" />
+                    </div>
+                  </div>
+
+                  <button className="w-[90%] btnPrimary">Login</button>
+                </div>
+                <div className="">
+                  <div className="p-2 m-4 text-2xl text-center border rounded-xl w-fit text-cl-black">
+                    OR
+                  </div>
+                </div>
+                <Link to='/forgot-password' className="hover:border-0 btnLink hover:link">
+                  Forgot password ?
+                </Link>
+              </form>
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="mt-6 form-control">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
+            <Link
+              to="/register"
+              className="p-4 bg-cl-acn text-white rounded-b-[20px] w-60  -mt-2 hover:text-cl-acn hover:bg-cl-white border border-cl-acn text-center text-lg flex flex-col"
+            >
+              <span className="text-xs label-text-alt">Don`t have an account ?</span>
+              <button className="font-semibold">Register</button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
