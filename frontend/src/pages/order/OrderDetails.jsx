92403e00331ad3ab22422e7b08824292f74de5d3
Oke Babatunde <tundejosilnet@gmail.com>
2024-01-21 23:04:31 +0100

    order receipt

--- frontend/src/pages/order/OrderDetails.jsx
+++ frontend/src/pages/order/OrderDetails.jsx
@@ -1,18 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getOrder } from "../../redux/features/order/orderSlice";
 import { BsArrowLeft } from "react-icons/bs";
 import { SpinnerImg } from "../../components/Loader";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import Logo from "../../assets/logo.png";
 
 const OrderDetails = () => {
+  const [loader, setLoader] = useState(false);
+  const pdfRef = useRef();
   const { id } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { isLoading, order } = useSelector((state) => state.order);
 
   useEffect(() => {
     dispatch(getOrder(id));
   }, [dispatch, id]);
+
+  const downloadPDF = () => {
+    // const capture = document.querySelector(".container");
+    // setLoader(true);
+    // html2canvas(capture).then((canvas) => {
+    //   const imgData = canvas.toDataURL("img/png");
+    //   const doc = new jsPDF("p", "mm", "a4", true);
+    //   const componentWidth = doc.internal.pageSize.getWidth();
+    //   const componentHeight = doc.internal.pageSize.getHeight();
+    //   doc.addImage(imgData, "PNG", 0, 0, componentWidth, componentHeight);
+    //   setLoader(false);
+    //   doc.save(`shopitoInvoice.pdf`);
+    // });
+
+    const input = pdfRef.current;
+    setLoader(true);
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imageWidth = canvas.width;
+      const imageHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imageWidth, pdfHeight / imageHeight);
+      const imgX = (pdfWidth - imageWidth * ratio) / 2;
+      const imgY = 30;
+      pdf.addImage(
+        imgData,
+        "PNG",
+        imgX,
+        imgY,
+        imageWidth * ratio,
+        imageHeight * ratio
+      );
+      setLoader(false);
+      pdf.save(`OjaAppInvoice.pdf`);
+    });
+  };
 
   return (
     <section>
@@ -25,7 +69,20 @@ const OrderDetails = () => {
           <BsArrowLeft />
           <p>Go back to Order</p>
         </Link>
-        <div>
+        <div className="mt-2" ref={pdfRef}>
+          <div className="flex flex-col items-center pb-2">
+            <div className="flex items-center justify-center text-2xl font-semibold text-cl-black">
+              <img src={Logo} alt="home" className="w-12 h-12 rounded-full" />
+              <h5 className="px-2 -ml-[8px] pb-1">Oja</h5>
+            </div>
+            <a href="http://google.com" className="link">
+              oja
+            </a>
+            <p>0123456789</p>
+            <p>Your online shop</p>
+            <h3 className="text-2xl">Your order(s) with us</h3>
+          </div>
+
           {isLoading && order === null ? (
             <SpinnerImg />
           ) : (
@@ -67,65 +124,78 @@ const OrderDetails = () => {
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col justify-center w-full mt-2 text-center">
-                <div className=" max-h-[28rem] overflow-y-scroll mb-3  border rounded-lg py-4">
-                  {order?.cartItems.map((cart, index) => {
-                    const { _id, name, price, image, cartQuantity } = cart;
-                    return (
-                      <div
-                        className="px-1 py-1 m-2 duration-300 rounded-lg bg-cl-sec md:px-4 hover:shadow-lg hover:my-3 hover:py-2"
-                        key={_id}
-                      >
-                        <p className="text-xl font-bold text-end text-cl-acn">
-                          {index + 1}.
-                        </p>
-                        <>
-                          <h5 className="pl-2 font-semibold text-center uppercase md:text-xl md:text-start">
-                            {name}
-                          </h5>
-                          <div className="flex items-center justify-between gap-4 px-2 py-1 md:py-0">
-                            <div className="flex flex-col items-center md:gap-4 md:flex-row">
-                              <div className="flex flex-col items-center text-center md:text-end">
-                                <img
-                                  src={image[0]}
-                                  alt={name}
-                                  className="object-cover w-20 h-20 m-1 md:m-2 rounded-xl"
-                                />
-                                <p className="">
-                                  Price : ${" "}
-                                  <span className=" md:text-xl">{price}</span>
-                                </p>
-                              </div>
-                            </div>
-                            <div className="flex flex-col items-center gap-4">
-                              <div className="flex flex-col gap-2 md:gap-6 md:flex-row">
-                                <p>
-                                  Total Items :
-                                  <span className="h3">{cartQuantity}</span>
-                                </p>
-                                <h2>
-                                  Total Amount : $
-                                  <span className="h3">
-                                    {(cartQuantity * price).toFixed(2)}
-                                  </span>
-                                </h2>
-                              </div>
-                              <Link to={`/review-product/${_id}`}>
-                                <button className="w-full btnPrimary">
-                                  Review Product
-                                </button>
-                              </Link>
+              <div className="flex flex-col justify-center w-full my-2 text-center">
+                {order?.cartItems.map((cart, index) => {
+                  const { _id, name, price, image, cartQuantity } = cart;
+                  return (
+                    <div
+                      className="px-1 py-1 m-2 duration-300 rounded-lg bg-cl-sec md:px-4 "
+                      key={_id}
+                    >
+                      <p className="text-xl font-bold text-start text-cl-acn">
+                        {index + 1}.
+                      </p>
+                      <>
+                        <h5 className="pl-2 font-semibold text-center uppercase md:text-xl md:text-start">
+                          {name}
+                        </h5>
+                        <div className="flex items-center justify-between gap-4 px-2 py-1 md:py-0">
+                          <div className="flex flex-col items-center md:gap-4 md:flex-row">
+                            <div className="flex flex-col items-center text-center md:text-end">
+                              <img
+                                src={image[0]}
+                                alt={name}
+                                className="object-cover w-20 h-20 m-1 md:m-2 rounded-xl"
+                              />
+                              <p className="">
+                                Price : ${" "}
+                                <span className=" md:text-xl">{price}</span>
+                              </p>
                             </div>
                           </div>
-                        </>
-                      </div>
-                    );
-                  })}
-                </div>
+                          <div className="flex flex-col items-center gap-4">
+                            <div className="flex flex-col gap-2 md:gap-6 md:flex-row">
+                              <p>
+                                Total Items :
+                                <span className="h3">{cartQuantity}</span>
+                              </p>
+                              <h2>
+                                Total Amount : $
+                                <span className="h3">
+                                  {(cartQuantity * price).toFixed(2)}
+                                </span>
+                              </h2>
+                            </div>
+                            <Link to={`/review-product/${_id}`}>
+                              <button className="w-full btnPrimary">
+                                Review Product
+                              </button>
+                            </Link>
+                          </div>
+                        </div>
+                      </>
+                    </div>
+                    
+                  );
+                })}
               </div>
+              {/* <h3 className="">Thank you for your patronage</h3> */}
             </div>
           )}
+        
         </div>
+
+        <button
+          disabled={!(loader === false)}
+          onClick={downloadPDF}
+          className="w-full border-2 rounded-lg border-cl-acn text-cl-acn"
+        >
+          {loader ? (
+            <span className="m-1 loading loading-spinner loading-md"></span>
+          ) : (
+            <p className="border-0 btnPrimary">Download as PDF</p>
+          )}
+        </button>
       </div>
     </section>
   );
